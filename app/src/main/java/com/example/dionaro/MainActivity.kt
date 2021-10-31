db957cc3bada8162d096d242e930ad9df49adc61
abigail <mariana.aby@gmail.com>
2021-10-30 23:04:35 -0500

    Layout de Inicio terminada con elementos básicos

--- app/src/main/java/com/example/dionaro/MainActivity.kt
+++ app/src/main/java/com/example/dionaro/MainActivity.kt
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private val perfilCreado = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (perfilCreado){
+            setContentView(R.layout.activity_inicio)
+        }else{
+            setContentView(R.layout.activity_login)
+        }
     }
+
 }
